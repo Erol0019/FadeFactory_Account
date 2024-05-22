d036820e8a07ca7ba7200a3b560996ef4f66e439
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2024-05-22 15:45:05 +0200

    Try catch for register

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -37,19 +37,30 @@ namespace FadeFactory_Accounts.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<Account>> CreateAccount(Account account)
         {
-            int dbSize = (await _service.GetAllAccounts()).LastOrDefault()?.AccountId ?? 0;
-            account.AccountId = dbSize + 1;
+            try
+            {
+                int dbSize = (await _service.GetAllAccounts()).LastOrDefault()?.AccountId ?? 0;
+                account.AccountId = dbSize + 1;
 
-            Account createdAccount = await _service.CreateAccount(account);
+                Account createdAccount = await _service.CreateAccount(account);
 
-            var url = Url.RouteUrl("GetAccountByIdAsync", new { createdAccount.AccountId }, Request.Scheme);
-            System.Console.WriteLine(url);
+                var url = Url.RouteUrl("GetAccountByIdAsync", new { createdAccount.AccountId }, Request.Scheme);
+                System.Console.WriteLine(url);
 
-            String host = HttpContext.Request.Host.Value;
-            String uri = $"https://{host}/api/Accounts/{createdAccount.AccountId}";
+                String host = HttpContext.Request.Host.Value;
+                String uri = $"https://{host}/api/Accounts/{createdAccount.AccountId}";
 
-            return Created(uri, createdAccount);
-            //return CreatedAtAction(nameof(GetAccountByIdAsync), new { createdAccount.Id }, createdAccount);
+                return Created(uri, createdAccount);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Account with email already exists."))
+                {
+                    return Conflict(new { message = "Email is already in use" });
+                }
+
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete("{AccountId}")]

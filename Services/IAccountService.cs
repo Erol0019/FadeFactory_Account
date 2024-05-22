4b4694f78475c66583b8156a40de874ee871d582
Snaflus <7541911+Snaflus@users.noreply.github.com>
2024-05-22 20:05:05 +0200

    namespace indent

--- Services/IAccountService.cs
+++ Services/IAccountService.cs
@@ -2,16 +2,15 @@ using FadeFactory_Accounts.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace FadeFactory_Accounts.Services
+namespace FadeFactory_Accounts.Services;
+public interface IAccountService
 {
-    public interface IAccountService
-    {
-        Task<Account> GetAccount(int AccountId);
-        Task<IEnumerable<Account>> GetAllAccounts();
-        Task<Account> CreateAccount(Account account);
-        Task<Account> UpdateAccount(Account account);
-        Task DeleteAccount(int AccountId);
-        Task<Account> RegisterAccount(Account account);
-        Task<string> Login(Account loginRequest);
-    }
+    Task<Account> GetAccount(int AccountId);
+    Task<IEnumerable<Account>> GetAllAccounts();
+    Task<Account> CreateAccount(Account account);
+    Task<Account> UpdateAccount(Account account);
+    Task DeleteAccount(int AccountId);
+    Task<Account> RegisterAccount(Account account);
+    Task<string> Login(Account loginRequest);
 }
+
